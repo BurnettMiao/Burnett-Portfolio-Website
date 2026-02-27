0a3d884ae904e5e4d7fa1698b015b2267c469f4d
BurnettMiao <searchfor1987@gmail.com>
2026-02-27 17:19:45 +0800

    命名調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import Main from '@/components/Main.vue'
-import AboutMe from '@/components/AboutMe.vue'
+import MainSection from '@/components/MainSection.vue'
+import AboutMeSection from '@/components/AboutMeSection.vue'
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto p-5">
-    <Main />
-    <AboutMe />
+    <MainSection />
+    <AboutMeSection />
     <div>作品集主頁重構中...</div>
   </div>
 </template>
